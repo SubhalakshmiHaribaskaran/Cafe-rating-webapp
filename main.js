c6ccce4040aaf7120766222813edff19c8be205d
SubhalakshmiHaribaskaran <subhahari7@gmail.com>
2024-03-08 19:32:29 +0530

    Get request setup

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
 
+app.get("/",(req,res) => {
+  res.send("Hellos");
+})
 app.listen(PORT,() =>{
   console.log("App is listening on "+PORT);
 });
