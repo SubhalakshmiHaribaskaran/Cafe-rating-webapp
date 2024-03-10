43e130af26f6aca1e122ba11082517951ffe351f
SubhalakshmiHaribaskaran <subhahari7@gmail.com>
2024-03-10 16:06:51 +0530

    Added the data name-collection name checking if its working right

--- config/dbConnectionConfig.js
+++ config/dbConnectionConfig.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 const dbConnection = async () =>{
   try{
-    const connect = await mongoose.connect("mongodb+srv://admin:admin@subhacluster.tcaekj0.mongodb.net/?retryWrites=true&w=majority&appName=subhacluster");
+    const connect = await mongoose.connect("mongodb+srv://admin:admin@subhacluster.tcaekj0.mongodb.net/cafe_data?retryWrites=true&w=majority&appName=subhacluster");
+    console.log("Database connected ",connect.connection.host,connect.connection.name);
+  }
+  catch{
+    console.log("Failed to connect the database");
+    process.exit(1);
   }
 }
+
+module.exports = dbConnection;
