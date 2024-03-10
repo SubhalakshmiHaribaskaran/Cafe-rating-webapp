8ade53cccfedc421d030ab62ec8721c9826389ac
SubhalakshmiHaribaskaran <subhahari7@gmail.com>
2024-03-10 20:40:08 +0530

    We want to add the entered data into database schema

--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Object = require("../models/model");
 
 const router = express.Router();
 
@@ -6,9 +7,21 @@ router.get("/",(req,res) => {
   res.send("Hellos");
 });
 // Creating a post request , getting value from the user and doing what we want to do
-router.post("/add", (req,res) =>{
-  console.log(req.body);
-})
+router.post("/add", async(req,res) =>{
+  try{
+    const cafe = new Object({
+      name : req.body.name,
+      phone : req.body.phone,
+      reviewSum : 0,
+      reviewCount :0
+    });
+    await cafe.save();
+    console.log("Save the new cafe:"+ cafe);
+  }
+  catch{
+    console.log("Error while adding a new cafe"); 
+  }
+});
 // To test this route we are using thunderclient 
 
 module.exports = router;
