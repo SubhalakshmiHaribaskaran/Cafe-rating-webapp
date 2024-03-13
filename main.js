a068eaf838078107e20a75473fb099056597ed26
SubhalakshmiHaribaskaran <subhahari7@gmail.com>
2024-03-13 14:41:31 +0530

    copy paste from github

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const express = require('express');
-// value can also be writtern as express
+const express = require("express");
 const dotenv = require("dotenv").config();
 const db = require("./config/dbConnectionConfig");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const session = require("express-session");
 
-const PORT = process.env.PORT;
 const app = express();
+const PORT = process.env.PORT;
 
 db();
-// session middleware
+
+//Middlewares
 app.use(
     session({
         secret : "my secret key",
@@ -22,19 +22,16 @@ app.use((req,res,next)=>{
     delete req.session.message;
     next();
 });
-// Add body parser middleware
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// middleware
 app.use(express.json());
-app.use("/",require("./routes/route"));
+app.use("/", require("./routes/route"));
 
-// to use ejs template
+
+
 app.set("view engine","ejs");
 
-app.get("/test",(req,res) =>{
-  res.send("Test yaaaaaaaaaaaa");
-})
-app.listen(PORT,() =>{
-  console.log("App is listening on "+PORT);
-}); 
+
+app.listen(PORT,()=>{
+    console.log("App is live on the port ... : " + PORT);
+});
--- routes/route.js
+++ routes/route.js
@@ -3,79 +3,82 @@ const Object = require("../models/model");
 
 const router = express.Router();
 
-router.get("/", async (req,res) => {
-  try{
-    const cafes = await Object.find();
-    res.render("home" , {
-      title:"Home Page",
-      data : cafes
-    });
-  }
-  catch(error){
-    console.log("errors");
-  }
-  // [{"_id":"65edccc34d80fceb03c2641a","name":"Pizzahut","phone":"232174","reviewSum":0,"reviewCount":0,"__v":0},
-  // {"_id":"65edcd2757d3bec4c7702723","name":"Dominoz","phone":"334455","reviewSum":0,"reviewCount":0,"__v":0},
-  // {"_id":"65eea878782d01e8cc708db7","name":"Pizzahut","phone":"232174","reviewSum":0,"reviewCount":0,"__v":0}]
+router.get("/", async (req,res)=>{
+    try{
+        const cafes = await Object.find();
+        res.render("home",{
+            title : "Home Page",
+            data : cafes
+        });
+    }
+    catch(error){
+        console.log("Error while getting values from DataBase");
+    }
 });
-// Creating a get method for /add
-router.get("/add",(req,res)=>{
-  res.render("add_cafe",{
-    title:"Add New Cafe"
-  });
-})
-// Creating a post request , getting value from the user and doing what we want to do
-router.post("/add", async(req,res) =>{
-  console.log("name : "+ req.body.name);
-  try{
-    const cafe = new Object({
-      name : req.body.name,
-      phone : req.body.phone,
-      reviewSum : 0,
-      reviewCount :0
+
+router.get("/add", (req,res)=>{
+    res.render("add_cafe",{
+        title : "Add New Cafe"
     });
-    await cafe.save();
-    console.log("Save the new cafe:"+ cafe);
-    res.redirect("/");
-  }
-  catch{
-    console.log("Error while adding a new cafe"); 
-  }
 });
-// To test this route we are using thunderclient 
+
+router.post("/add", async (req,res)=>{
+    console.log("name : " + req.body.name);
+    try{
+        const cafe = new Object({
+            name : req.body.name,
+            phone : req.body.phone,
+            reviewSum : 0,
+            reviewCount : 0
+        });
+        await cafe.save();
+        req.session.message = {
+            type : "success",
+            info : "You have added a new cafe successfully"
+        }
+        res.redirect("/");
+    }
+    catch{
+        console.log("Error while adding a new cafe");
+    }
+});
 
 router.get("/rate/:id", async (req,res)=>{
-  const id = req.params.id;
-  try{
-    const cafe = await Object.findById(id);
-    res.render("rate_cafe",{
-      title : "Rate Cafe",
-      cafe : cafe
-    });
-  }catch(error){
-    console.log("Some error occured");
-  }
+    const id = req.params.id;
+    try{
+        const cafe = await Object.findById(id);
+        res.render("rate_cafe",{
+            title : "Rate Cafe",
+            cafe : cafe
+        });
+    }
+    catch(err){
+        console.log("Some error occured while getting the data of cafe");
+    }
 });
-// Creating a POST route
+
 router.post("/rate/:id", async (req,res)=>{
-  const id = req.params.id;
-  const rating = parseInt(req.body.rating);
-  try{
-    const cafe = await Object.findById(id);
-    const newSum = parseInt(cafe.reviewSum) + rating;
-    const newCount = parseInt(cafe.reviewCount) + 1;
-    await Object.findByIdAndUpdate(id,{
-      reviewSum : newSum,
-      reviewCount : newCount
-    });
-    res.redirect("/");
-    res.render("rate_cafe",{
-      title : "Rate Cafe",
-      cafe : cafe
-    });
-  }catch(error){
-    console.log("Some error occured");
-  }
+    const id = req.params.id;
+    const rating = parseInt(req.body.rating);
+    try{
+        const cafe = await Object.findById(id);
+        const newSum = parseInt(cafe.reviewSum) + rating;
+        const newCount = parseInt(cafe.reviewCount) + 1;
+        await Object.findByIdAndUpdate(id,{
+            reviewSum : newSum,
+            reviewCount : newCount
+        });
+
+        req.session.message = {
+            type : "success",
+            info : "You have rated successfully"
+        }
+
+        res.redirect("/");
+    }
+    catch(err){
+        console.log("Some error occured while getting the data of cafe");
+    }
 });
 
 module.exports = router;
