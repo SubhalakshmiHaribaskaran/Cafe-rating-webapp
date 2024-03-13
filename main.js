f11a65457c57a013a6fcdb49699917dfc224e7ab
SubhalakshmiHaribaskaran <subhahari7@gmail.com>
2024-03-13 14:26:19 +0530

    Notification logic - Create a session

--- main.js
+++ main.js
@@ -3,12 +3,25 @@ const express = require('express');
 const dotenv = require("dotenv").config();
 const db = require("./config/dbConnectionConfig");
 const bodyParser = require('body-parser');
+const session = require("express-session");
 
 const PORT = process.env.PORT;
 const app = express();
 
 db();
-
+// session middleware
+app.use(
+    session({
+        secret : "my secret key",
+        saveUninitialized : true,
+        resave: false,
+    })
+);
+app.use((req,res,next)=>{
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+});
 // Add body parser middleware
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
@@ -24,4 +37,4 @@ app.get("/test",(req,res) =>{
 })
 app.listen(PORT,() =>{
   console.log("App is listening on "+PORT);
-});
+}); 
